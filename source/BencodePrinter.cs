f1771f8ba02f9c92cc3b044c0a4be3172e2c1f7d
emil ksenzovski <e.ksenzovski@gmail.com>
2017-12-06 17:27:07 +0200

    first commit

---
+++ source/BencodePrinter.cs
@@ -0,0 +1,58 @@
+﻿using BencodeNET.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace source
+{
+    // prints bencode objects
+    class BencodePrinter
+    {
+        // prints bencode list
+        public void PrintBlist(BList list)
+        {
+            foreach (var item in list)
+            {
+                if (item is BDictionary)
+                    PrintBdict(item as BDictionary);
+                else if (item is BList)
+                    PrintBlist(item as BList);
+                else
+                    Console.WriteLine($"item => {item}");
+            }
+        }
+
+        // prints bencode dictionary
+        public void PrintBdict(BDictionary dict)
+        {
+            foreach (var pair in dict)
+            {
+                Console.WriteLine($"key => {pair.Key}");
+
+                if (pair.Value is BDictionary)
+                    PrintBdict(pair.Value as BDictionary);
+
+                else if (pair.Value is BList)
+                    PrintBlist(pair.Value as BList);
+
+                else
+                    Console.WriteLine($"value => {pair.Value}");
+            }
+        }
+
+        // prints bencode generic object
+        public void Print(BObject BObj)
+        {
+            if (BObj is BDictionary)
+                PrintBdict(BObj as BDictionary);
+
+            else if (BObj is BList)
+                PrintBlist(BObj as BList);
+
+            else
+                Console.WriteLine(BObj);
+        }
+    }
+}
---
+++ source/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BencodeNET;
+using BencodeNET.Parsing;
+using BencodeNET.Torrents;
+using System.IO;
+using BencodeNET.Objects;
+
+namespace source
+{
+    class Program
+    {
+        
+        static string fileLoc = @"C:\Users\User\Desktop\textExample.torrent";
+
+        static void Main(string[] args)
+        {
+            // Parse torrent by specifying the file path
+            var parser = new BencodeParser(); // Default encoding is Encoding.UT8F, but you can specify another if you need to
+            var torrent = parser.Parse<Torrent>(fileLoc);
+
+            /*
+            // Alternatively, handle the stream yourself
+            using (var stream = File.OpenRead(fileLoc))
+            {
+                torrent = parser.Parse<Torrent>(stream);
+            }
+            */
+
+            // Calculate the info hash
+            string infoHash = torrent.GetInfoHash();
+            Console.WriteLine($"infoHash => {infoHash}");
+            
+
+            // or as bytes instead of a string
+            byte[] infoHashBytes = torrent.GetInfoHashBytes();
+
+            // Get Magnet link
+            string magnetLink = torrent.GetMagnetLink();
+            Console.WriteLine($"magnetLink => {magnetLink}");
+            
+            // create object to print th fields in the torrent file
+            BencodePrinter Bprinter = new BencodePrinter();
+
+            // Convert Torrent to its BDictionary representation
+            BDictionary bencodeDict = torrent.ToBDictionary();
+            Console.WriteLine(bencodeDict.Count);
+
+            try
+            {
+                Bprinter.PrintBdict(bencodeDict);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine("DONE!!!!!!!");       
+            Console.ReadKey();
+            
+        }
+    }
+}
